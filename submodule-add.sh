e1e42130b226239bf5fa9ae7b40fc0e2961d1dc0
weixiaoxing <weixiaoxing@meishubao.com>
2020-03-23 17:38:07 +0800

    init

---
+++ submodule-add.sh
@@ -0,0 +1,10 @@
+#!/usr/bin bash
+git submodule add https://github.com/WeisonWei/e-school-service-user-oauth2-db.git
+git submodule add https://github.com/WeisonWei/e-school-service-user-oauth2-mem.git
+git submodule add https://github.com/WeisonWei/e-school-service-user-oauth2-jwt.git
+git submodule add https://github.com/WeisonWei/e-school-service-user-oauth2-rbac.git
+git submodule add https://github.com/WeisonWei/e-school-service-user-oauth2-resource.git
+
+#submodule add 执行完后执行
+#git submodule init
+#git submodule update
